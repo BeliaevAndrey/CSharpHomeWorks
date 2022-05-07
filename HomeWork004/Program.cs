e6a32163e267922a741af8ca9828a626c914bf90
Andrei Beliaev <abbot190414@gmail.com>
2022-05-07 14:38:36 +0300

    HW4 Task 3 solved

--- HomeWork004/Program.cs
+++ HomeWork004/Program.cs
@@ -34,6 +34,7 @@ Console.WriteLine("[{0}] -> {1}", string.Join(", ", array), CountEven(array));
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
+/*
 int[] FillArray(int len)
 {
     int[] outarr = new int[len];
@@ -59,8 +60,39 @@ Console.Write("Задайте длину массива: ");
 int arlen = Convert.ToInt32(Console.ReadLine());
 int [] array = FillArray(arlen);
 Console.WriteLine("[{0}] -> {1}", string.Join(", ", array), SumOddPositions(array));
-
+*/
 
 
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
+
+/*
+double[] FillArray(int len)
+{
+    double[] outarr = new double[len];
+    for(int i = 0; i < len; i++)
+    {
+        outarr[i] = Convert.ToDouble(new Random().Next(0, 101));
+    }
+    return outarr;
+}
+
+double DiffMaxMin(double[] arr)
+{
+    double max = arr[0], min = arr[0];
+    
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(max < arr[i]) max = arr[i];
+        if(min > arr[i]) min = arr[i];
+    }
+    Console.WriteLine("max = " + max);
+    Console.WriteLine("min = " + min);
+    return max - min;
+}
+
+Console.Write("Задайте длину массива: ");
+int arlen = Convert.ToInt32(Console.ReadLine());
+double [] array = FillArray(arlen);
+Console.WriteLine("[{0}] -> {1}", string.Join(" ", array), DiffMaxMin(array));
+*/
