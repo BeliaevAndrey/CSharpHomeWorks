cba4315db00ae5b122fd921ba958291fdd9b828d
Andrei Beliaev <abbot190414@gmail.com>
2022-05-20 23:17:04 +0300

    HW 9 Task 1 started

--- HomeWork009/Program.cs
+++ HomeWork009/Program.cs
@@ -5,46 +5,55 @@ using System.Threading;
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-/*
-int SumOfNaturals(int m, int n)
+
+int SumOfNaturalsControl(int m, int n)
 {
     int k = n;
     for(int i = m; i < k; i++)
     {
         Console.WriteLine(i + "\t" + m + "\t" + n);
         m += (n--) ;
-        Console.WriteLine(i + "\t" + m + "\t" + n);
     }
     return m;
 }
 
 
-// int SumOfNaturals(int m, int n)
-// {
-//     if(n != m)
-//     {
-//         Console.WriteLine(m + "\t" + n);
-//         return SumOfNaturals(m, n - 1);
-//     }
-//     return m;
-// }
+int SumOfNaturals(int m, int n)
+{
+    int result = n;
+    if(n != m)
+    {
+        Console.WriteLine(m + "\t" + n);
+        return SumOfNaturals(m, n - 1);
+    }
+    return m;
+}
 
-Console.WriteLine("Введите первое целое число M: ");
-int startNum = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе целое число N: ");
-int finNum = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите первое целое число M: ");
+// int startNum = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе целое число N: ");
+// int finNum = Convert.ToInt32(Console.ReadLine());
+
+var (startNum, finNum) = (1, 15);
+
 Console.WriteLine();
-Console.WriteLine($"{startNum}; {finNum} ->  {SumOfNaturals(startNum, finNum)}");
+Console.WriteLine($"{startNum}; {finNum} -> {SumOfNaturalsControl(startNum, finNum)}");
 
 
-*/
+// (startNum, finNum) = (1, 15);
+
+Console.WriteLine();
+Console.WriteLine($"{startNum}; {finNum} -> {SumOfNaturals(startNum, finNum)}");
+
+
 
 // Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
 // 453 -> 12
 // 45 -> 9
 
 
-int SumOfDigits(int n, int result = 0)
+
+int SumOfDigits(int n, int result)
 {
     if(n / 10 == 0)
         return result;
@@ -55,5 +64,6 @@ int SumOfDigits(int n, int result = 0)
 
 Console.WriteLine("Введите целое число: ");
 int number = Convert.ToInt32(Console.ReadLine());;
-Console.WriteLine("Сумма: " + number + " -> " + SumOfDigits(number, number%10));
+Console.WriteLine("Сумма: " + number + " -> " + SumOfDigits(number, number % 10));
+
 
