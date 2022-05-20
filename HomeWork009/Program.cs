0a39a8d7bfb075fffa231efaf43c222c8bd8e3fe
Andrei Beliaev <abbot190414@gmail.com>
2022-05-20 20:45:19 +0300

    HW 9 Task 2 solved

--- HomeWork009/Program.cs
+++ HomeWork009/Program.cs
@@ -1,18 +1,33 @@
-﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+﻿using System;
+using System.Threading;
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
-using System;
 
+/*
 int SumOfNaturals(int m, int n)
 {
-    if(n != m)
+    int k = n;
+    for(int i = m; i < k; i++)
     {
-        Console.WriteLine(m + "\t" + n);
-        // m += n;
-        return SumOfNaturals(m + n, 1);
+        Console.WriteLine(i + "\t" + m + "\t" + n);
+        m += (n--) ;
+        Console.WriteLine(i + "\t" + m + "\t" + n);
     }
     return m;
 }
+
+
+// int SumOfNaturals(int m, int n)
+// {
+//     if(n != m)
+//     {
+//         Console.WriteLine(m + "\t" + n);
+//         return SumOfNaturals(m, n - 1);
+//     }
+//     return m;
+// }
 
 Console.WriteLine("Введите первое целое число M: ");
 int startNum = Convert.ToInt32(Console.ReadLine());
@@ -22,11 +37,23 @@ Console.WriteLine();
 Console.WriteLine($"{startNum}; {finNum} ->  {SumOfNaturals(startNum, finNum)}");
 
 
-
+*/
 
 // Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
 // 453 -> 12
 // 45 -> 9
 
 
+int SumOfDigits(int n, int result = 0)
+{
+    if(n / 10 == 0)
+        return result;
+    else
+        return SumOfDigits(n /= 10, result += n % 10);
+}
+
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());;
+Console.WriteLine("Сумма: " + number + " -> " + SumOfDigits(number, number%10));
 
