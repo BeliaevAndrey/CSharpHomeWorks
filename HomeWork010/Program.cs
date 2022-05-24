12d6f63f70198c486c72b57651b4d04c29256dcb
Andrei Beliaev <abbot190414@gmail.com>
2022-05-24 03:38:39 +0300

    HW 10 solved completely

--- HomeWork010/Program.cs
+++ HomeWork010/Program.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections;
 
 // Задача 1: Задайте массив строк. Напишите программу, считает кол-во слов в массиве, начинающихся на гласную букву.
 // Пример: { "qwe", "wer", "ert", "rty", "tyu"} -> 1
 
-
+/*
 
 int VowelCount(string[] array)
 {
@@ -13,7 +12,7 @@ int VowelCount(string[] array)
 
     foreach(string word in array)
         foreach(char letter in vowels)
-            if(word.StartsWith(letter))
+            if(word.ToLower().StartsWith(letter))
                 count++;
 
     return count;
@@ -24,21 +23,23 @@ Console.Write("{" + string.Join(", ", firstTaskArray1) + "} -> ");
 Console.WriteLine(VowelCount(firstTaskArray1));
 Console.WriteLine();
 
-string[] firstTaskArray2 = { "ewq", "wer", "ert", "ytr", "tyu"};
+string[] firstTaskArray2 = { "ewq", "wer", "Ert", "ytr", "tyu"};
 Console.Write("{" + string.Join(", ", firstTaskArray2) + "} -> ");
 Console.WriteLine(VowelCount(firstTaskArray2));
 Console.WriteLine();
 
+*/
 
 // Задача 2: Задайте массив строк. Напишите программу, которая генерирует новый массив, объединяя элементы исходного массива попарно.
 // Пример: { "qwe", "wer", "ert", "rty", "tyu", "yui"} -> { "qwewer", "ertrty", "tyuyui"}
 
 /*
+
 string[] PairConcat(string[] array)
 {
     int arLen = array.Length / 2 + array.Length % 2;
     string[] outArray = new string[arLen];
-    for(int i = 0, j = 0; i < array.Length -1; i += 2, j++)
+    for(int i = 0, j = 0; i < array.Length - 1; i += 2, j++)
     {
         outArray[j] = (array[i] + array[i + 1]);
         if(array.Length % 2 != 0 && j == arLen - 2)
@@ -83,6 +84,7 @@ string[] testArray3 = { "Jo", "hn",
 
 
 Console.WriteLine("{\"" + string.Join("\", \"", testArray)+ "\"" + " -> " + "\"" + string.Join("\", \"", PairConcat(testArray)) + "\"}");
+
 Console.WriteLine();
 Console.WriteLine("{\"" + string.Join("\", \"", testArray2)+ "\"" + "} -> {" + "\"" + string.Join("\", \"", PairConcat(testArray2)) + "\"}");
 
